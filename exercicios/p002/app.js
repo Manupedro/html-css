23350e820978c1ac5ec22e9e277b8cb5f144721c
Manupedro <manupedroc2@gmail.com>
2023-01-07 22:57:13 -0300

    conversor para pblicações

---
+++ exercicios/p002/app.js
@@ -0,0 +1,10 @@
+
+    
+    var btn = document.querySelector('button')
+    btn.onclick = function() {
+        var input = document.querySelector("#comprimento");
+
+        var texto = input.value / 10;
+        console.log(texto)
+        
+      }
