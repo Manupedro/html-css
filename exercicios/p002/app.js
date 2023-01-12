3d60f7fa718bb00b87028d6320b2d8c3dbf41083
Manupedro <manupedroc2@gmail.com>
2023-01-11 23:10:45 -0300

    atualizar contagem
    atualizar a contantagem usando o proprio input

--- exercicios/p002/app.js
+++ exercicios/p002/app.js
@@ -1,10 +1,15 @@
 
-    
-    var btn = document.querySelector('button')
-    btn.onclick = function() {
-        var input = document.querySelector("#comprimento");
-        var resposta = document.querySelector("#resposta");
 
-        var x = input.value / 10;
-        resposta.innerHTML= x*150
-      }
+var btn = document.querySelector('button')
+var input = document.querySelector("#comprimento");
+  var resposta = document.querySelector("#resposta");
+btn.onclick = function () {
+  var x = input.value / 10;
+
+  resposta.innerHTML = Math.round(x * 150)
+}
+
+input.onkeyup = function(){
+  var x = input.value / 10;
+  resposta.innerHTML = Math.round(x * 150)
+}
